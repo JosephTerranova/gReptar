097f70b449e5277c3c8843e297e6d87705920f4a
administrator <administrator@DEV1>
2025-06-02 22:58:46 -0400

    Add updated code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tipsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -49,6 +58,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(583, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // menuStrip1
             // 
@@ -63,12 +73,24 @@
             // 
             // fileToolStripMenuItem
             // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // helpToolStripMenuItem
             // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tipsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
             this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
             this.helpToolStripMenuItem.Text = "Help";
@@ -94,38 +116,21 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 57);
+            this.label2.Location = new System.Drawing.Point(32, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.Size = new System.Drawing.Size(65, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Text";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 98);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(800, 355);
-            this.listBox1.TabIndex = 5;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.label2.Text = "Search Text";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(692, 23);
+            this.button1.Location = new System.Drawing.Point(692, 27);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(27, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(692, 60);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(27, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "...";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -133,19 +138,66 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(63, 56);
             this.button3.TabIndex = 8;
-            this.button3.Text = "RawR!!";
+            this.button3.Text = "GREP";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(800, 355);
+            this.listBox1.TabIndex = 9;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 26);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(103, 26);
+            // 
+            // copyToolStripMenuItem1
+            // 
+            this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+            this.copyToolStripMenuItem1.Size = new System.Drawing.Size(102, 22);
+            this.copyToolStripMenuItem1.Text = "Copy";
+            // 
+            // tipsToolStripMenuItem
+            // 
+            this.tipsToolStripMenuItem.Name = "tipsToolStripMenuItem";
+            this.tipsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tipsToolStripMenuItem.Text = "Tips";
+            this.tipsToolStripMenuItem.Click += new System.EventHandler(this.tipsToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
@@ -158,6 +210,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,10 +226,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tipsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,24 +9,118 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace gReptar
 {
     public partial class Form1 : Form
     {
+        private ContextMenuStrip listBoxContextMenu;
+        private ToolStripMenuItem copyMenuItem;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            button3.Enabled = false;            
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
 
+            listBoxContextMenu = new ContextMenuStrip();
+            copyMenuItem = new ToolStripMenuItem("Copy");
+            copyMenuItem.Click += copyToolStripMenuItem_Click;
+            listBoxContextMenu.Items.Add(copyMenuItem);
+            listBox1.ContextMenuStrip = listBoxContextMenu;
+            listBox1.DoubleClick += listBox1_DoubleClick;
+
+
+        }
+
+        private void CheckTextboxes()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                button3.Enabled = true;
+            }
+            else
+            {
+                button3.Enabled = false;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }    
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+
+            await Task.Run(() =>
+            {
+
+                String directory = textBox1.Text;
+                String searchString = textBox2.Text;
+
+                Runspace runspace = RunspaceFactory.CreateRunspace();
+                runspace.Open();
+                Pipeline pipeline = runspace.CreatePipeline();
+                pipeline.Commands.AddScript($"$directory = '{directory}'");
+                pipeline.Commands.AddScript($"$searchString = '{searchString}'");
+                pipeline.Commands.AddScript(@"Get-ChildItem -Path $directory -Recurse -File | Where-Object { $_.Extension -notin '.exe', '.bin', '.dll' } | ForEach-Object { Select-String -Path $_.FullName -Pattern $searchString -SimpleMatch}");
+                var results = pipeline.Invoke();
+                runspace.Close();
+
+                listBox1.Invoke(new MethodInvoker(delegate
+                {
+                    listBox1.Items.Clear();
+                    foreach (var result in results)
+                    {
+                        if (result != null)
+                            listBox1.Items.Add(result.ToString());
+                    }
+                    listBox1.HorizontalScrollbar = true;
+
+                    int maxWidth = 0;
+                    using (Graphics g = listBox1.CreateGraphics())
+                    {
+                        foreach (var item in listBox1.Items)
+                        {
+                            int itemWidth = (int)g.MeasureString(item.ToString(), listBox1.Font).Width;
+                            if (itemWidth > maxWidth)
+                                maxWidth = itemWidth;
+                        }
+                    }
+                    listBox1.HorizontalExtent = maxWidth;
+
+                    button3.Enabled = true;
+                }));
+            });
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CheckTextboxes();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Select a folder";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CheckTextboxes();
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -34,33 +128,95 @@ namespace gReptar
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
-
-            String directory = textBox1.Text;
-            String searchString = textBox2.Text;
-
-            Runspace runspace = RunspaceFactory.CreateRunspace();
-            runspace.Open();
-            Pipeline pipeline = runspace.CreatePipeline();
-            pipeline.Commands.AddScript($"$directory = '{directory}'");
-            pipeline.Commands.AddScript($"$searchString = '{searchString}'");
-            pipeline.Commands.AddScript(@"Get-ChildItem -Path $directory -Recurse -File | Where-Object { $_.Extension -notin '.exe', '.bin', '.dll' } | ForEach-Object { Select-String -Path $_.FullName -Pattern $searchString -SimpleMatch | ForEach-Object { $_.Line }}");
-            var results = pipeline.Invoke();
-            runspace.Close();
-            
-            listBox1.Items.Clear();
-            foreach (var result in results)
+            if (keyData == (Keys.Control | Keys.C))
             {
-                if (result != null)
-                    listBox1.Items.Add(result.ToString());
+                if (listBox1.Focused && listBox1.SelectedItems.Count > 0)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var item in listBox1.SelectedItems)
+                    {
+                        sb.AppendLine(item.ToString());
+                    }
+                    Clipboard.SetText(sb.ToString());
+                }
+                return true; // key handled
             }
 
+            return base.ProcessCmdKey(ref msg, keyData);
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var item in listBox1.SelectedItems)
+                {
+                    sb.AppendLine(item.ToString());
+                }
+                Clipboard.SetText(sb.ToString());
+            }
+        }
 
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+
+            string selectedLine = listBox1.SelectedItem.ToString();
+
+            // Match full Windows path + line number + line content
+            // Example: C:\folder\file.txt:12:some line text
+            var match = Regex.Match(selectedLine, @"^(.+?):(\d+):");
+
+            if (!match.Success || match.Groups.Count < 3) return;
+
+            string filePath = match.Groups[1].Value;
+            if (!int.TryParse(match.Groups[2].Value, out int lineNumber)) return;
+
+            try
+            {
+                // Try opening in Notepad++ with line number
+                string notepadPlusPlusPath = @"C:\Program Files\Notepad++\notepad++.exe";
+                if (System.IO.File.Exists(notepadPlusPlusPath))
+                {
+                    System.Diagnostics.Process.Start(notepadPlusPlusPath, $"-n{lineNumber} \"{filePath}\"");
+                }
+                else
+                {
+                    // Fallback to default app
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo()
+                    {
+                        FileName = filePath,
+                        UseShellExecute = true
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open file:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tipsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("If Notepad++ is installed, double clicking on a line item will open the file AND jump to the correct line!");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = "https://github.com/JosephTerranova/gReptar",
+                UseShellExecute = true
+            });
         }
     }
 }
